f2c872f4bd94cd7506637f0bdc706ab0331934e2
Younes <khalfii.youunes@gmail.com>
2021-11-15 09:59:45 +0100

    Update form.js

--- js/form.js
+++ js/form.js
@@ -8,7 +8,6 @@ const carAPIURLs = {
   carrosserie:  'cars/marque/model/alimentation/chevaux/carrosseries',
   version:  'cars/marque/model/alimentation/chevaux/carrosserie/versions',
 };
-const formData = {};
 
 $(function() {
   const ids = Object.keys(carAPIURLs);
@@ -56,7 +55,7 @@ $(function() {
       $('#spinner').remove();
     
       const jsonRes = JSON.parse(res);
-      let html = `<option value=""></option>`;
+      let html = `<option value="">Veuillez sélectionner</option>`;
       if (id === 'year') {
         optionsValues = jsonRes.marquesPopular.map(x => x.marque).sort();
         for (const optionsValue of optionsValues)
@@ -71,6 +70,8 @@ $(function() {
         const routeArray = carAPIURLs[ids[index + 1]].split('/');
         const plural = routeArray[routeArray.length - 1];
         optionsValues = [...jsonRes[plural].map(x => x[ids[index + 1]])].sort();
+        // if (optionsValues.length === 1)
+        //  html = '';
         for (const optionsValue of optionsValues)
           html += `<option value="${optionsValue}">${isNaN(optionsValue) ? capitalize(optionsValue) : optionsValue}</option>`;
       }
@@ -90,7 +91,7 @@ $(function() {
   })
     
   $('#submit').on('click', function() {
-    $.ajax({ type: "POST", url: "http://example.com/form.php", data: formData, dataType: 'json',
+    $.ajax({ type: "POST", url: "https://assurancezenith.com:99/devis-api/v2/src/public/auto", data: getFormData(), dataType: 'json',
       success: function(data) { console.log("success", data); },
       error: function(error) { console.log("error", error); }
     })
@@ -104,4 +105,102 @@ function capitalize(input) {
     capitalizedWords.push(element.charAt(0).toUpperCase() + element.slice(1, element.length));
   });
   return capitalizedWords.join(' ');
+}
+
+const datesSinistres = [...$('[name=dateSinistre36DerniersMois').map(function () { return $(this).val() })];
+const naturesSinistres = [...$('[name=natureSinistre36DerniersMois').map(function () { return $(this).val() })];
+
+const sinistres = datesSinistres.map((date, i) => ({
+  dateSinistre36DerniersMois: date,
+  natureSinistre36DerniersMois: naturesSinistres[i],
+}))
+
+function getFormData() {
+  return {
+    vehicule: {
+      yearDateMEC: $('#dateMEC').val().split('-')[0],
+      dateMEC: $('#dateMEC').val(),
+      marque: $('#marque').val(),
+      model: $('#model').val(),
+      alimentation: $('#alimentation').val(),
+      chevaux: $('#chevaux').val(),
+      carrosserie: $('#carrosserie').val(),
+      version: $('#version').val(),
+      stationnement: $('#stationnement').val(),
+      usage: $('#usage').val(),
+    },
+    conducteurPrincipal: {
+      antecedents: {
+        isEteAssure36DerniersMois: $('#isEteAssure36DerniersMois').val(),
+        bonusMalus: $('#bonusMalus').val().split(' ')[0],
+        isEteResilie36DerniersMois: $('#isEteResilie36DerniersMois').val(),
+        motifResiliation36DerniersMois: $('#motifResiliation36DerniersMois').val(),
+        dateResiliation36DerniersMois: $('#dateResiliation36DerniersMois').val(),
+        isEuSinistres36DerniersMois: $('#isEuSinistres36DerniersMois').val(),
+        sinistres36DerniersMois: (() => {
+          const datesSinistres = [...$('[name=dateSinistre36DerniersMois').map(function () { return $(this).val() })];
+          const naturesSinistres = [...$('[name=natureSinistre36DerniersMois').map(function () { return $(this).val() })];
+      
+          return datesSinistres.map((date, i) => ({
+            dateSinistre36DerniersMois: date,
+            natureSinistre36DerniersMois: naturesSinistres[i],
+          }))
+        })(),
+        // alcoolemie: { 
+        // 	sanction: {
+        // 		dateInitialPermisApresAnnulation: "2020-11-1",
+        // 		dateAnnulationPermis36DerniersMois: "2020-11-1",
+        // 		dateSanction36DerniersMois: "2020-11-1",
+        // 		dateSuspensionPermis36DerniersMois: "2020-11-1",
+        // 		dateInfraction36DerniersMois: "2020-11-1",
+        // 		typeSuspensionPermis36DerniersMois: "Suspension permis",
+        // 		typeSanction: "Suspension permis",
+        // 		suspensionPermisMotif36DerniersMois: "Alcoolemie",
+        // 		nombreSuspensionPermis36AvecSinistreDerniersMois: 1,
+        // 		nombreSuspensionPermis36SansSinistreDerniersMois: 2,
+        // 		nombreMoisSuspensionPermis36DerniersMois: 6
+        // 	},
+        // 	dateDernierAlcoolemie36DerniersMois: "2020-11-1",
+        // 	nombreControlesPositifsAlcoolemie36DerniersMois: 1,
+        // 	isRefusTestAlcoolemie36DerniersMois: "Non",
+        // 	isDelitDeFuiteAlcoolemie36DerniersMois: "Oui",
+        // 	controleAlcoolemieSuiteA36DerniersMois: "Controle routier",
+        // 	controleAlcoolemieMethode36DerniersMois: "prise de sang",
+        // 	tauxAlcoolemie: "2.0",
+        // 	isAnnulationOuSuspensionPermis36DerniersMois: "suspension_permis"
+        // },
+        // stupefiant: {
+        // 	sanction: {
+        // 		typeAnnulationPermis36DerniersMois: "Annulation permis",
+        // 		typeSanction: "Annulation permis",
+        // 		annulationPermisMotif36DerniersMois: "Stupefiant",
+        // 		annulationPermisAvecOuSansSinistre36DerniersMois: "Sans sinistre"
+        // 	},
+        // 	dateDernierStupefiant36DerniersMois: "2020-11-1",
+        // 	nombreControlesPositifsStupefiants36DerniersMois: 1,
+        // 	isRefusTestStupefiant36DerniersMois: "Non",
+        // 	isDelitDeFuiteStupefiant36DerniersMois: "Non",
+        // 	controleStupefiantSuiteA36DerniersMois: "Controle routier",
+        // 	controleStupefiantMethode36DerniersMois: "prise de sang",
+        // 	isAnnulationOuSuspensionPermis36DerniersMois: "annulation_permis"
+        // }
+      },
+      dateNaissance: $('#dateNaissance').val(),
+      datePermis: $('#datePermis').val(),
+      civilite: $('#civilite').val(),
+      nom: $('#nom').val(),
+      prenom: $('#prenom').val(),
+      situationFamiliale: $('#situationFamiliale').val(),
+      adresse: $('#adresse').val(),
+      codePostal: $('#codePostal').val(),
+      ville: $('#ville').val(),
+      tel: $('#tel').val(),
+      email: $('#email').val()
+    },
+    urlSource: "https://zassauto.github.io/Auto-V1.2/form.html", 
+    deviceType: "D", 
+    titulaireCarteGrise: "Conducteur Principal", 
+    garantieSouhaiteeConducteurPrincipal: $('#garantieSouhaiteeConducteurPrincipal').val(), 
+    isAjouterConducteurSecondaire: "Non" 
+  }
 }
